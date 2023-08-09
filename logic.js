0e1c0cd7ec72b04f0ba371c18e890a89b0d6edb5
Jawad Shaikh <J.shk1980@gmail.com>
2023-08-09 14:33:52 -0400

    built more javascript to potentially make the fame work with the DOM

--- logic.js
+++ logic.js
@@ -1,5 +1,6 @@
 
 const computerimage = document.querySelector('.computer-image');
+const announcement = document.querySelector('.announcement')
 function getComputerChoice(){
     //get random number between 1 and 3
     choice = Math.floor(Math.random() * (3 - 1 + 1)) + 1
@@ -22,26 +23,25 @@ function playRound(playerSelection,computerSelection){
     player = playerSelection.toLowerCase()
     computer = computerSelection.toLowerCase()
     if (player == computerSelection){
-        return "TIE!"
+        announcement.textContent = "TIE"
     }
     else if (player == "rock" && computer == "scissors"){
-        return "You Win! rock beats scissors!"
+        announcement.textContent = "You Win! rock beats scissors!"
     }
     else if (player == "rock" && computer == "paper"){
-        return "You Lose! paper beats rock"
+        announcement.textContent = "You Lose! paper beats rock"
     }
     else if (player == "paper" && computer == "scissors"){
-        return "You Lose! Scissors beat paper!"
+        announcement.textContent = "You Lose! Scissors beat paper!"
     }
     else if (player == "paper" && computer == "rock"){
-        return "You Win! paper beats rock"
+        announcement.textContent = "You Win! paper beats rock"
     }
     else if (player == "scissors" && computer == "paper"){
-        return "You Win! scissors beats paper!"
+        announcement.textContent ="You Win! scissors beats paper!"
     }
     else if (player == "scissors" && computer == "rock"){
-        return "You Lose! rock beats scissors"
+        announcement.textContent = "You Lose! rock beats scissors"
     }
 }
-
 
