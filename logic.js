866b2bed13a58846bb91654a687958fb7dd71f41
Jawad Shaikh <J.shk1980@gmail.com>
2023-08-09 15:45:58 -0400

    built all the css and html trying to make the game work gonna do some googling or something

--- logic.js
+++ logic.js
@@ -1,47 +1,52 @@
 
-const computerimage = document.querySelector('.computer-image');
-const announcement = document.querySelector('.announcement')
 function getComputerChoice(){
     //get random number between 1 and 3
     choice = Math.floor(Math.random() * (3 - 1 + 1)) + 1
     //based on the value 1-3 the function returns rock paper or scissors
     if (choice == 1){
-        computerimage.setAttribute('src','imagesForRPS/fistemojiproper.png')
+        return 'rock'
     }
     else if (choice == 2){
-        computerimage.setAttribute('src','imagesForRPS/handemojigood.png')
+        return 'paper'
     }
     else if (choice == 3){
-        computerimage.setAttribute('src','imagesForRPS/scissorsemojigood.png')
+        return 'scissors'
     }
     else{
-        getComputerChoice()
+        return 'scissors'
     }
 }
-
+const announcement = document.querySelector('h2')
 function playRound(playerSelection,computerSelection){
     player = playerSelection.toLowerCase()
     computer = computerSelection.toLowerCase()
     if (player == computerSelection){
         announcement.textContent = "TIE"
+        return "TIE"
     }
     else if (player == "rock" && computer == "scissors"){
-        announcement.textContent = "You Win! rock beats scissors!"
+        return "You Win!"
     }
     else if (player == "rock" && computer == "paper"){
-        announcement.textContent = "You Lose! paper beats rock"
+        announcement.textContent = "You Lose!"
+        return "You Lose!"
+
     }
     else if (player == "paper" && computer == "scissors"){
-        announcement.textContent = "You Lose! Scissors beat paper!"
+        announcement.textContent = "You Lose!"
+        return "You Lose!"
     }
     else if (player == "paper" && computer == "rock"){
-        announcement.textContent = "You Win! paper beats rock"
+        announcement.textContent = "You Win!"
+        return "You Win!"
     }
     else if (player == "scissors" && computer == "paper"){
-        announcement.textContent ="You Win! scissors beats paper!"
+        announcement.textContent = "You Win"
+        return "You Win!"
     }
     else if (player == "scissors" && computer == "rock"){
-        announcement.textContent = "You Lose! rock beats scissors"
+        announcement.textContent = "You Lose!"
+        return "you Lose !"
     }
 }
-
+playRound('rock',getComputerChoice())
