99962c1449b50f139419e525f2f90c35beae3be9
Jawad Shaikh <J.shk1980@gmail.com>
2023-08-09 21:46:51 -0400

    made the buttons function the scoreboards and Round number but need to limit the rounds and a reset button too

--- logic.js
+++ logic.js
@@ -4,7 +4,15 @@ const btnscissors = document.querySelector("#scissors")
 const btnpaper = document.querySelector("#paper")
 const computerimage = document.querySelector("#computer-image")
 const playerimage = document.querySelector("#player-image")
+const playerScore = document.querySelector("#player-score")
+const computerScore = document.querySelector("#computer-score")
+const roundNumber = document.querySelector("#roundNum")
 
+let playerScoreHtml = 0;
+let computerScoreHtml = 0;
+let scoreString = "Score: "
+let roundString = "Round: "
+let ROUNDNUM = 0
 function getComputerChoice(){
     //get random number between 1 and 3
     choice = Math.floor(Math.random() * (3 - 1 + 1)) + 1
@@ -30,41 +38,75 @@ function playRound(playerSelection,computerSelection){
     player = playerSelection.toLowerCase()
     computer = computerSelection.toLowerCase()
     if (player == computerSelection){
-        announcement.textContent = 'TIE';
+        ROUNDNUM++
+        roundNumber.textContent = roundString + ROUNDNUM
     }
     else if (player == "rock" && computer == "scissors"){
-        announcement.textContent = "You Win!"
+        playerScoreHtml++;
+        playerScore.textContent = scoreString+playerScoreHtml
+        ROUNDNUM++
+        roundNumber.textContent = roundString + ROUNDNUM
     }
     else if (player == "rock" && computer == "paper"){
-        announcement.textContent = "You Lose!"
-
+        computerScoreHtml++;
+        computerScore.textContent = scoreString+computerScoreHtml;
+        ROUNDNUM++
+        roundNumber.textContent = roundString + ROUNDNUM
     }
     else if (player == "paper" && computer == "scissors"){
-        announcement.textContent = "You Lose!"
+        computerScoreHtml++;
+        computerScore.textContent = scoreString+computerScoreHtml;
+        ROUNDNUM++
+        roundNumber.textContent = roundString + ROUNDNUM
     }
     else if (player == "paper" && computer == "rock"){
-        announcement.textContent = "You Win!"
+        playerScoreHtml++;
+        playerScore.textContent = scoreString+playerScoreHtml;
+        ROUNDNUM++
+        roundNumber.textContent = roundString + ROUNDNUM
     }
     else if (player == "scissors" && computer == "paper"){
-        announcement.textContent = "You Win!"
+        playerScoreHtml++;
+        playerScore.textContent = scoreString+playerScoreHtml
+        ROUNDNUM++
+        roundNumber.textContent = roundString + ROUNDNUM
     }
     else if (player == "scissors" && computer == "rock"){
-        announcement.textContent = "You Lose!"
+        computerScoreHtml++;
+        computerScore.textContent = scoreString+computerScoreHtml;
+        ROUNDNUM++
+        roundNumber.textContent = roundString + ROUNDNUM
     }
 }
+
+announcement.textContent = "Choose an option to play"
 
 btnpaper.addEventListener("click", function(){
     playerimage.setAttribute('src','imagesForRPS/handemoji.png')
     computer = getComputerChoice()
     playRound('paper', computer);
-})
+    })
 btnrock.addEventListener('click', function(){
     playerimage.setAttribute('src','imagesForRPS/fistemojiproper.png')
     computer = getComputerChoice()
     playRound('rock',computer)
-})
+    })
 btnscissors.addEventListener('click',function(){
     playerimage.setAttribute('src','imagesForRPS/scissorsemojigood.png')
     computer = getComputerChoice()
     playRound('scissors',computer)
-})
+    })
+
+    if (ROUNDNUM >= 5){
+        alert("GAME OVER")
+    }
+    
+    if (computerScoreHtml > playerScoreHtml){
+        announcement.textContent = "!!! YOU WIN !!!"
+    } 
+    else if (playerScoreHtml > computerScoreHtml){
+        announcement.textContent = "!!! YOU LOSE !!! "
+    }
+    else{
+        announcement.textContent = "TIE"
+    }
